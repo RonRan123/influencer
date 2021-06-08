715a7b47913421e83ece92cae6d9bae0de9d36f3
RonRan123 <>
2021-06-08 14:33:48 -0400

    paused work on mainBlog

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,16 +1,18 @@
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
 import React from 'react';
 
 function PostCard({info, image}){
     return(
+        // On click should take you to the post
         <div key={image.id} className="masonry-item" >
             <img 
                 src={image.urls.small}
                 alt={image.description}
             />
-            <div>
-                <p>Title</p>
-            </div>
+            {/* <div className="post-info">
+                <Button variant="contained">Show Post</Button>
+            </div> */}
+            {/* <div class="button"><a href="#"> BUTTON </a></div> */}
         </div>
     )
 }
