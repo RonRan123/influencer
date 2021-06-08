ce1bbb811a451a20634ff68134067f125d7b545e
adrielk <theadrielkim@gmail.com>
2021-06-08 11:48:37 -0400

    navbar componentization

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import FeaturedGrid from './FeaturedGrid'
+import HomeNavbar from './HomeNavbar'
 import NavigationDrawer from './NavigationDrawer'
-import FBLogo from '../../icons/facebook-logo.svg'
-import InstaLogo from '../../icons/insta-logo.svg'
-import TwitterLogo from '../../icons/twitter-logo.svg'
 import '../../css/home-page.css'
-import {
-    Button,
-    Input,
-    IconButton 
-}from '@material-ui/core'
-import SearchIcon from '@material-ui/icons/Search';
 //To do:
 //Use media queries to set breakpoints for full screen navbar view
 
@@ -36,39 +28,12 @@ function HomePage() {
             <h1>
                 Camille's Corner
             </h1>
+            <HomeNavbar/>
             <div className="menu-button">
-                    <NavigationDrawer/>
-                </div>
-            <div className="navbar">
-                <div className="navbutton-group">
-                    <div className="navbutton--border">
-                        <Button classes={{label:"navbutton-label"}}>Home</Button>
-                    </div>
-                    <div className="navbutton">
-                        <Button classes={{label:"navbutton-label"}}>Blog</Button>
-                    </div>
-                    <div className="navbutton">
-                        <Button classes={{label:"navbutton-label"}}>Forum</Button>
-                    </div>
-                    <div className="navbutton">
-                        <Button classes={{label:"navbutton-label"}}>Shop</Button>
-                    </div>
-               
-                </div>
-                <div className="search-bar">
-                    <IconButton> 
-                        <SearchIcon/>
-                    </IconButton>
-                    <Input disableUnderline={true} classes={{root:"search-bar__form"}} id="search-input" placeholder={"Search"}/>
-                </div>
-                <img onClick={()=>{console.log("fb link")}} className="social-icon" src={FBLogo}/>
-                <img className="social-icon" src={InstaLogo}/>
-                <img className="social-icon" src={TwitterLogo}/>
-
-                <Button classes={{label:"navbutton-label"}}>Login</Button>
+                <NavigationDrawer/>
             </div>
-            <FeaturedGrid/>
 
+            <FeaturedGrid/>
         </div>
     )
 }
