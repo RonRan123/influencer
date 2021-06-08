f15e9ddf483ef88daee641bf0d3f53dcdb36fc14
adrielk <theadrielkim@gmail.com>
2021-06-08 11:36:26 -0400

    homepage css tweaks

--- src/components/home/FeaturedGrid.js
+++ src/components/home/FeaturedGrid.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import {
+    Button
+}from '@material-ui/core'
 
 const blurbData = `Camille is a world-famous model who has only ever wanted to be normal, 
 even though her mother has always pushed her to become a superstar. Now, she's a college dropout,
@@ -11,6 +14,11 @@ function FeaturedGrid({blogPosts}) {
     const randomURL2 = 'https://picsum.photos/450/200'
     const randomURL3 = 'https://picsum.photos/450/200'
 
+    const buttonStyle={
+        margin:0,
+        padding:0,
+        borderRadius:"10px"
+    }
 
     return (
         <div className="featured-blogs">
@@ -23,14 +31,22 @@ function FeaturedGrid({blogPosts}) {
             </div>
             <div className="featured-container--short">
                 <h2>Latest Blog Post</h2>
-                <img className="featured-img--short" src={randomURL2}/>
+                <Button style={buttonStyle}>
+                    <img className="featured-img--short" src={randomURL2}/>
+                    {/* <div className="overlaid-text">overlain text</div> */}
+
+                </Button>
             </div>
             <div className="featured-container--short">
                 <h2>Latest Merchandise</h2>
-                <img className="featured-img--short" src={randomURL3}/>
+                <Button style={buttonStyle}>
+                    <img className="featured-img--short" src={randomURL3}/>
+                </Button>
             </div>
             <div className="featured-container--short">
-                <img className="featured-img--short" src={randomURL3}/>
+                <Button style={buttonStyle}>
+                    <img className="featured-img--short" src={randomURL3}/>
+                </Button>
             </div>
         </div>
     )
