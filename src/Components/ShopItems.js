b6a470891f5a05e01ed387ee0d33fa7cbef692fe
JakeMondschein1 <jhm4by@virginia.edu>
2021-06-08 16:41:14 -0400

    Update ShopItems.js

--- src/Components/ShopItems.js
+++ src/Components/ShopItems.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ShopCard from './ShopCard';
 import Paper from '@material-ui/core/Paper';
+import './shop-page.css'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -20,7 +21,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function ShopItems(){
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
     const [selectedItem, setSelectedItem] = React.useState();
 
     const [cart, setCart] = React.useState([]);
@@ -33,16 +33,18 @@ export default function ShopItems(){
 //mapping through the shopping data 
 //putting the fields into different parts of a MUI card
 //wrapping it in a grid  
-    return(           
+    return( 
+        <div className = 'background'>         
         <div className={classes.root}>
             <Grid container spacing={1} justify="center"> 
-                {shopData && shopData.map((item, index) => <div> <Paper className={classes.paper}> <ShopCard id={index} info={item}/> <div><br></br></div><Button variant="contained" onClick={() => handleAdd(item.title)}>Add to Cart</Button></Paper><br></br></div>)}
+                {shopData && shopData.map((item, index) => <div> <Paper className={classes.paper}> <ShopCard id={index} info={item}/> <div><br></br></div><Button className='cart-button' variant='contained' onClick={() => handleAdd(item.title)}>Add to Cart</Button></Paper><br></br></div>)}
             </Grid>
             <br></br>
             <div>
                 <h2>Cart = {cart} </h2>
             </div>  
-        </div>     
+        </div>  
+        </div>    
     );  
 
 }
