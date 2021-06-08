7fac26d045b389c9f46c53f5fecc3709d36ac81f
adrielk <theadrielkim@gmail.com>
2021-06-08 17:38:59 -0400

    comment and typo

--- src/Components/ShopItems.js
+++ src/Components/ShopItems.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import shopData from "./ShopData";
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
