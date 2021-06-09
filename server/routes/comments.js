23fe236e89025a1204e22edfd533e8dd542a2891
RonRan123 <>
2021-06-09 10:32:12 -0400

    get, put, post, and delete comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -11,4 +11,41 @@ router.get("/get", async (req, res) => {
     res.json(comments);
 });
 
-module.export = router;
+router.post("/add", async (req, res) => {
+    const { content, date, dislikes, likes, postID, user, ...rest } = req.body;
+    const resp = await db.collection("comments").add({
+        content, 
+        date, 
+        dislikes, 
+        likes, 
+        postID, 
+        user
+    });
+  
+    console.log("Added document to comments with ID: ", resp.id);
+    res.sendStatus(200);
+  });
+
+
+router.delete("/delete", async (req, res) => {
+    const {doc_id, ...rest} = req.body;
+    const resp = await db.collection("comments").doc(doc_id).delete();
+    console.log("From comments, deleted: ", doc_id);
+    res.send("Got a DELETE request for comments");
+});
+
+router.put("/update", async (req, res) => {
+    const { doc_id, content, date, dislikes, likes, postID, user, ...rest } = req.body;
+    const resp = await db.collection("comments").doc(doc_id).update({
+        content: content, 
+        date: date, 
+        dislikes: dislikes, 
+        likes: likes, 
+        postID: postID, 
+        user: user
+    });
+  
+    res.send("Got a put request to update a comment");
+});
+
+module.exports = router;
